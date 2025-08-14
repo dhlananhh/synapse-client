182119ede599869d64d14c6bed477e7b94fa2299
Lan Anh <dhlananh2309@gmail.com>
2025-08-15 00:11:35 +0700

    feat: implement UserNav component

---
+++ src/components/shared/UserNav.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useAuth } from "@/context/AuthContext";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button";
+import { UserAvatar } from "./UserAvatar";
+
+export default function UserNav() {
+  const { currentUser, logout } = useAuth();
+
+  if (!currentUser) return null;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+          <UserAvatar user={ currentUser } />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">{ currentUser.username }</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              { currentUser.id }@synapse.io
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Settings</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={ logout }>
+          Log out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}

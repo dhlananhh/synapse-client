19694efb80ff0deea02d9b01ea6ec2b1b3be921e
Lan Anh <dhlananh2309@gmail.com>
2025-08-15 00:13:37 +0700

    feat: add MainLayout component

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,18 @@
+import Navbar from "@/components/shared/Navbar";
+
+export default function MainLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="relative flex min-h-screen flex-col">
+      <Navbar />
+
+      <main className="container mx-auto max-w-7xl pt-12 flex-grow">
+        { children }
+      </main>
+
+    </div>
+  );
+}

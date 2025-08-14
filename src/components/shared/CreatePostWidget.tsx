310d4ac3a57a6b88a684cf1fd49ecfeb20b36e67
Lan Anh <dhlananh2309@gmail.com>
2025-08-15 00:11:58 +0700

    feat: implement CreatePostWidget component

---
+++ src/components/shared/CreatePostWidget.tsx
@@ -0,0 +1,40 @@
+// File: src/components/shared/CreatePostWidget.tsx
+"use client";
+
+import { useAuth } from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { UserAvatar } from "./UserAvatar";
+import { Image, LinkIcon } from "lucide-react";
+
+
+export default function CreatePostWidget() {
+  const router = useRouter();
+  const { currentUser } = useAuth();
+
+  // On click, navigate to the submit page
+  const goToSubmitPage = () => router.push("/submit");
+
+  return (
+    <div className="mb-8 p-3 rounded-md bg-card flex items-center gap-4 border">
+      { currentUser && <UserAvatar user={ currentUser } /> }
+
+      <div className="relative w-full">
+        <Input
+          onClick={ goToSubmitPage }
+          readOnly
+          placeholder="Create a new post..."
+          className="cursor-pointer"
+        />
+      </div>
+
+      <Button onClick={ goToSubmitPage } variant="ghost" size="icon" title="Create Image Post">
+        <Image className="h-5 w-5" />
+      </Button>
+      <Button onClick={ goToSubmitPage } variant="ghost" size="icon" title="Create Link Post">
+        <LinkIcon className="h-5 w-5" />
+      </Button>
+    </div>
+  )
+}

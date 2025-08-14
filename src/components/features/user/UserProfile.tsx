596fc411f10e54a0ed5fd45e80c7916b10820d42
Lan Anh <dhlananh2309@gmail.com>
2025-08-15 00:28:52 +0700

    feat: implement user profile component

---
+++ src/components/features/user/UserProfile.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { User, Post, UserComment } from "@/types";
+import ProfileHeader from "./ProfileHeader";
+import UserPostFeed from "./UserPostFeed";
+import UserCommentFeed from "./UserCommentFeed";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+interface UserProfileProps {
+  user: User;
+  userPosts: Post[];
+  userComments: UserComment[];
+}
+
+export default function UserProfile({ user, userPosts, userComments }: UserProfileProps) {
+  return (
+    <div>
+      <ProfileHeader user={ user } postCount={ userPosts.length } />
+
+      <div className="mt-6">
+        <Tabs defaultValue="posts" className="w-full">
+          <TabsList>
+            <TabsTrigger value="posts">Posts</TabsTrigger>
+            <TabsTrigger value="comments">Comments</TabsTrigger>
+          </TabsList>
+          <TabsContent value="posts">
+            <UserPostFeed posts={ userPosts } />
+          </TabsContent>
+          <TabsContent value="comments">
+            <UserCommentFeed comments={ userComments } />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
+  )
+}

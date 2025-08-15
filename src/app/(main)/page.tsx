9efe9f30722c8cfc3962f27ec8b42579f1d4d89e
Lan Anh <dhlananh2309@gmail.com>
2025-08-15 11:53:12 +0700

    feat: add "use client" directive to HomePage component

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,22 +1,15 @@
-import PostCard from "@/components/features/post/PostCard";
+"use client";
+
+import React from "react";
 import CreatePostWidget from "@/components/shared/CreatePostWidget";
 import PostFeed from "@/components/features/post/PostFeed";
-import { mockPosts } from "@/lib/mock-data";
+
 
 export default function HomePage() {
-  const posts = mockPosts;
-
   return (
     <div className="mt-10">
-
       <CreatePostWidget />
       <PostFeed />
-
-      {/* <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        { posts.map(post => (
-          <PostCard key={ post.id } post={ post } />
-        )) }
-      </div> */}
     </div>
   )
 } 

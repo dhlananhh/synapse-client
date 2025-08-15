0a9bbb755c6359ae6f50a655a725c4ad1322d426
Lan Anh <dhlananh2309@gmail.com>
2025-08-15 21:06:35 +0700

    feat: add margin top to feed page

--- src/app/(main)/feed/page.tsx
+++ src/app/(main)/feed/page.tsx
@@ -5,7 +5,7 @@ import PostFeed from "@/components/features/post/PostFeed";
 
 export default function FeedPage() {
   return (
-    <div>
+    <div className="mt-10">
       <CreatePostWidget />
       <PostFeed />
     </div>

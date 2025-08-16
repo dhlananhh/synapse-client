d397b9d526320975b5c39d6a01395d0409dff8cb
Lan Anh <dhlananh2309@gmail.com>
2025-08-17 01:28:57 +0700

    feat: integrate user activity calendar into user profile page

--- src/components/features/user/UserProfile.tsx
+++ src/components/features/user/UserProfile.tsx
@@ -1,21 +1,35 @@
 "use client"
 
-import { User, Post, UserComment } from "@/types";
+import {
+  User,
+  Post,
+  UserComment,
+  Activity
+} from "@/types";
 import ProfileHeader from "./ProfileHeader";
 import UserPostFeed from "./UserPostFeed";
 import UserCommentFeed from "./UserCommentFeed";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import ActivityCalendar from './ActivityCalendar';
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger
+} from "@/components/ui/tabs";
+
 
 interface UserProfileProps {
   user: User;
   userPosts: Post[];
   userComments: UserComment[];
+  activity: Activity[];
 }
 
-export default function UserProfile({ user, userPosts, userComments }: UserProfileProps) {
+export default function UserProfile({ user, userPosts, userComments, activity }: UserProfileProps) {
   return (
     <div className="mt-10">
       <ProfileHeader user={ user } postCount={ userPosts.length } />
+      <ActivityCalendar activityData={ activity } />
 
       <div className="mt-6">
         <Tabs defaultValue="posts" className="w-full">

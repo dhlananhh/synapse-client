9b546b4c45532e779d890a85ae163fdf0115b75d
Lan Anh <dhlananh2309@gmail.com>
2025-08-19 23:47:37 +0700

    feat: add margin top to settings page

--- src/app/(main)/settings/page.tsx
+++ src/app/(main)/settings/page.tsx
@@ -28,7 +28,7 @@ export default function SettingsPage() {
   }
 
   return (
-    <div>
+    <div className="mt-10">
       <h1 className="text-3xl font-bold mb-6">Settings</h1>
       <UpdateProfileForm />
     </div>

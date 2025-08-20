2d2dcc38b1e5f7f0d3f1e383276c2233e3e1ab22
Lan Anh <dhlananh2309@gmail.com>
2025-08-20 13:29:41 +0700

    feat: implement shadcn-ui components

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -20,10 +20,12 @@ function Command({
   return (
     <CommandPrimitive
       data-slot="command"
-      className={ cn(
-        "bg-popover text-popover-foreground flex h-full w-full flex-col overflow-hidden rounded-md",
-        className
-      ) }
+      className={
+        cn(
+          "bg-popover text-popover-foreground flex h-full w-full flex-col overflow-hidden rounded-md",
+          className
+        )
+      }
       { ...props }
     />
   )
@@ -89,10 +91,12 @@ function CommandList({
   return (
     <CommandPrimitive.List
       data-slot="command-list"
-      className={ cn(
-        "max-h-[300px] scroll-py-1 overflow-x-hidden overflow-y-auto",
-        className
-      ) }
+      className={
+        cn(
+          "max-h-[300px] scroll-py-1 overflow-x-hidden overflow-y-auto",
+          className
+        )
+      }
       { ...props }
     />
   )
@@ -117,10 +121,12 @@ function CommandGroup({
   return (
     <CommandPrimitive.Group
       data-slot="command-group"
-      className={ cn(
-        "text-foreground [&_[cmdk-group-heading]]:text-muted-foreground overflow-hidden p-1 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium",
-        className
-      ) }
+      className={
+        cn(
+          "text-foreground [&_[cmdk-group-heading]]:text-muted-foreground overflow-hidden p-1 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium",
+          className
+        )
+      }
       { ...props }
     />
   )
@@ -146,10 +152,12 @@ function CommandItem({
   return (
     <CommandPrimitive.Item
       data-slot="command-item"
-      className={ cn(
-        "data-[selected=true]:bg-accent data-[selected=true]:text-accent-foreground [&_svg:not([class*='text-'])]:text-muted-foreground relative flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
-        className
-      ) }
+      className={
+        cn(
+          "data-[selected=true]:bg-accent data-[selected=true]:text-accent-foreground [&_svg:not([class*='text-'])]:text-muted-foreground relative flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+          className
+        )
+      }
       { ...props }
     />
   )
@@ -162,10 +170,12 @@ function CommandShortcut({
   return (
     <span
       data-slot="command-shortcut"
-      className={ cn(
-        "text-muted-foreground ml-auto text-xs tracking-widest",
-        className
-      ) }
+      className={
+        cn(
+          "text-muted-foreground ml-auto text-xs tracking-widest",
+          className
+        )
+      }
       { ...props }
     />
   )
--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -16,10 +16,12 @@ function Separator({
       data-slot="separator"
       decorative={ decorative }
       orientation={ orientation }
-      className={ cn(
-        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
-        className
-      ) }
+      className={
+        cn(
+          "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
+          className
+        )
+      }
       { ...props }
     />
   )

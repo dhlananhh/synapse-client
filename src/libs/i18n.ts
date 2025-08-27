fdaa612eef0530022bb786985096627158ac84d6
Lan Anh <dhlananh2309@gmail.com>
2025-08-28 00:01:49 +0700

    feat: add support for German, French and Italian languages

--- src/libs/i18n.ts
+++ src/libs/i18n.ts
@@ -1,11 +1,16 @@
 "use client";
 
+
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+
 import en from "@/locales/en.json";
 import vi from "@/locales/vi.json";
 import de from "@/locales/de.json";
+import fr from "@/locales/fr.json";
+import it from "@/locales/it.json";
+
 
 i18n
   .use(LanguageDetector)
@@ -25,6 +30,12 @@ i18n
       },
       de: {
         translation: de,
+      },
+      fr: {
+        translation: fr,
+      },
+      it: {
+        translation: it,
       },
     },
   });

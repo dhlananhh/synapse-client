5fe656ba3bc68459617c2a75307f9e4687653876
Lan Anh <dhlananh2309@gmail.com>
2025-09-01 16:35:51 +0700

    feat: implement reset password page

---
+++ src/app/(auth)/reset-password/[token]/page.tsx
@@ -0,0 +1,8 @@
+import ResetPasswordForm from "@/components/features/auth/ResetPasswordForm";
+
+export default async function ResetPasswordPage({ params }) {
+
+  return (
+    <ResetPasswordForm params={ params } />
+  );
+}

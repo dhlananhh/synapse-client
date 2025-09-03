f91de6835ef647bb762c1f56f89310855f9a9e0d
Lan Anh <dhlananh2309@gmail.com>
2025-09-03 23:51:25 +0700

    feat: create user profile layout

---
+++ src/app/(main)/(user)/u/[username]/layout.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+
+import React from "react";
+import { useParams, notFound } from "next/navigation";
+import { allMockUsers, mockPosts } from "@/libs/mock-data";
+import ProfileHeader from "@/components/features/user/ProfileHeader";
+import TrophyCaseWidget from "@/components/features/user/TrophyCaseWidget";
+
+
+export default function UserProfileLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const params = useParams();
+  const username = typeof params.username === "string" ? params.username : "";
+
+  const user = allMockUsers.find(u => u.username === username);
+  const userPostCount = mockPosts.filter(p => p.author.username === username).length;
+
+  if (!user) {
+    return notFound();
+  }
+
+  return (
+    <div className="space-y-8 mt-10 mb-10">
+      <ProfileHeader user={ user } postCount={ userPostCount } />
+
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-y-6 lg:gap-x-6">
+
+        <div className="lg:col-span-2">
+          { children }
+        </div>
+
+        <aside className="lg:col-span-1">
+          <div className="sticky top-72 space-y-4">
+            <TrophyCaseWidget username={ user.username } />
+          </div>
+        </aside>
+      </div>
+    </div>
+  );
+}

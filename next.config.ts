d88ced0ac3b2b84a542a49c0bed6a553e745ddc6
Lan Anh <dhlananh2309@gmail.com>
2025-09-11 15:05:12 +0700

    feat: add image configuration for remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
+
 
 export default nextConfig;

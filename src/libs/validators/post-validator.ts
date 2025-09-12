cee979e562d45013da53981351ba937419b7bbda
Lan Anh <dhlananh2309@gmail.com>
2025-09-13 01:14:19 +0700

    feat: add flairId field to PostSchema

--- src/libs/validators/post-validator.ts
+++ src/libs/validators/post-validator.ts
@@ -1,11 +1,13 @@
 import { z } from "zod";
 
+
 export const PostSchema = z.object({
   title: z.string()
     .min(3, { message: "Title must be at least 3 characters long." })
     .max(128, { message: "Title must be no longer than 128 characters." }),
   communityId: z.string(),
   content: z.any(),
+  flairId: z.string().optional(),
 });
 
 export type TPostSchema = z.infer<typeof PostSchema>;

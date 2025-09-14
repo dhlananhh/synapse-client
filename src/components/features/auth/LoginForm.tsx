ddc2e1ec0ec009e01713a53409bc5830af21a3c2
Lan Anh <dhlananh2309@gmail.com>
2025-09-14 13:44:20 +0700

    style: code formatting

--- src/components/features/auth/LoginForm.tsx
+++ src/components/features/auth/LoginForm.tsx
@@ -22,7 +22,10 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { BrainCircuit, Loader2 } from "lucide-react";
+import {
+  BrainCircuit,
+  Loader2
+} from "lucide-react";
 
 
 export default function LoginForm() {

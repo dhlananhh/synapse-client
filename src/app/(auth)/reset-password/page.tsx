f86dd287bc669b3b490856134a92d3dd8bf97faf
Lan Anh <dhlananh2309@gmail.com>
2025-09-17 23:38:57 +0700

    refactor: remove token param from ResetPasswordPage

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -3,6 +3,6 @@ import ResetPasswordForm from "@/components/features/auth/ResetPasswordForm";
 export default async function ResetPasswordPage({ params }) {
 
   return (
-    <ResetPasswordForm params={ params } />
+    <ResetPasswordForm />
   );
 }

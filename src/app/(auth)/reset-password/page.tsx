0ab3951c4a6e062fed97c3130abc9c649906b086
Lan Anh <dhlananh2309@gmail.com>
2025-09-17 23:39:32 +0700

    refactor: simplify ResetPasswordPage component

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,7 +1,6 @@
 import ResetPasswordForm from "@/components/features/auth/ResetPasswordForm";
 
-export default async function ResetPasswordPage({ params }) {
-
+export default async function ResetPasswordPage() {
   return (
     <ResetPasswordForm />
   );

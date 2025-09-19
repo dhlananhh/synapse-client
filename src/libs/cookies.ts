daff98e16f28cfb3aa4d7437149bde4160e37e47
Lan Anh <dhlananh2309@gmail.com>
2025-09-19 11:24:04 +0700

    refactor: reimplement access token expiration and cookie update

--- src/libs/cookies.ts
+++ src/libs/cookies.ts
@@ -1,45 +1,17 @@
 import Cookies from "js-cookie";
 
-// Define cookie names as constants to avoid typos
-export const COOKIE_NAMES = {
-  ACCESS_TOKEN: "synapse_access_token",
-  REFRESH_TOKEN: "synapse_refresh_token",
-};
+const ACCESS_TOKEN_KEY = "accessToken";
 
-// Access Token
 export const getAccessToken = (): string | undefined => {
-  return Cookies.get(COOKIE_NAMES.ACCESS_TOKEN);
+  return Cookies.get(ACCESS_TOKEN_KEY);
 };
 
-export const setAccessToken = (token: string, options?: Cookies.CookieAttributes): void => {
-  Cookies.set(COOKIE_NAMES.ACCESS_TOKEN, token, {
-    expires: 1 / 96, // 15 minutes
-    ...options,
-  });
+export const setAccessToken = (token: string): void => {
+  // Access token expires in 5 minutes
+  const expires = new Date(new Date().getTime() + 5 * 60 * 1000);
+  Cookies.set(ACCESS_TOKEN_KEY, token, { expires });
 };
 
 export const removeAccessToken = (): void => {
-  Cookies.remove(COOKIE_NAMES.ACCESS_TOKEN);
-};
-
-// Refresh Token
-export const getRefreshToken = (): string | undefined => {
-  return Cookies.get(COOKIE_NAMES.REFRESH_TOKEN);
-};
-
-export const setRefreshToken = (token: string, options?: Cookies.CookieAttributes): void => {
-  Cookies.set(COOKIE_NAMES.REFRESH_TOKEN, token, {
-    expires: 1 / 96, // 15 minutes
-    ...options,
-  });
-};
-
-export const removeRefreshToken = (): void => {
-  Cookies.remove(COOKIE_NAMES.REFRESH_TOKEN);
-};
-
-// Clear all auth-related cookies on logout
-export const clearAuthCookies = (): void => {
-  removeAccessToken();
-  removeRefreshToken();
+  Cookies.remove(ACCESS_TOKEN_KEY);
 };
